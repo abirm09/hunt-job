08e3b6bd7aa000c7ba79aa0d4893c0f31c30f23f
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-04-10 22:35:58 +0600

    created error page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import { FaceFrownIcon } from "@heroicons/react/24/solid";
 const Error = () => {
   return (
-    <div>
-      <h2 className="text-center text-7xl">Error</h2>
+    <div
+      className=" bg-slate-100 flex items-center justify-center"
+      style={{ minHeight: "calc(100vh - 64px)" }}
+    >
+      <div className="flex flex-col items-center space-y-5">
+        <FaceFrownIcon className="h-28 w-28 text-gray-400" />
+        <h2 className="text-red-700 text-7xl">404</h2>
+        <p className="text-3xl font-bold text-slate-600">Page not found.</p>
+      </div>
     </div>
   );
 };
