ba4c84fc4de59c4524c4773c1a959667b70bbc20
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-04-11 11:39:49 +0600

    Added questions answers

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LoaderDataContext } from "../App";
 
 const Blog = () => {
+  const { answers } = useContext(LoaderDataContext || []);
+  console.log(answers);
   return (
-    <div>
-      <h2>from blog</h2>
-    </div>
+    <section>
+      <div className="bg-zinc-100">
+        <h2 className="section-title py-10">Blogs</h2>
+      </div>
+      <div className="max-w-7xl mx-auto px-3 mt-10">
+        <ul className="list-decimal ml-5">
+          {answers.map((answer, index) => (
+            <li key={index} className="mb-5 font-extrabold">
+              <h2 className="mb-2 ">{answer.question}</h2>
+              <p className="font-medium text-zinc-600">{answer.answer}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
 
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -6,26 +6,31 @@ const Statistics = () => {
   const { assignmentData } = useContext(LoaderDataContext || []);
   console.log(assignmentData);
   return (
-    <div className="max-w-7xl mx-auto">
-      <h2 className="text-center pt-10 font-extrabold">
-        Assignment marks from assignment 1 to assignment 8.
-      </h2>
-      <div className="flex justify-center">
-        <PieChart width={350} height={400}>
-          <Pie
-            dataKey="marks"
-            isAnimationActive={false}
-            data={assignmentData}
-            cx="50%"
-            cy="50%"
-            outerRadius={80}
-            fill="#8884d8"
-            label
-          ></Pie>
-          <Tooltip />
-        </PieChart>
+    <section>
+      <div className="bg-zinc-100">
+        <h2 className="section-title py-10">Statistics</h2>
       </div>
-    </div>
+      <div className="max-w-7xl mx-auto">
+        <h2 className="text-center pt-10 font-extrabold">
+          Assignment marks from assignment 1 to assignment 8.
+        </h2>
+        <div className="flex justify-center">
+          <PieChart width={350} height={400}>
+            <Pie
+              dataKey="marks"
+              isAnimationActive={false}
+              data={assignmentData}
+              cx="50%"
+              cy="50%"
+              outerRadius={80}
+              fill="#8884d8"
+              label
+            ></Pie>
+            <Tooltip />
+          </PieChart>
+        </div>
+      </div>
+    </section>
   );
 };
 
