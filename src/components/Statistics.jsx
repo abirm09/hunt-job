26f219cacc0dfb9e50a27ee70c285b47d27734c7
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-04-11 10:06:30 +0600

    added pie chart

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LoaderDataContext } from "../App";
+import { Pie, PieChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
 
 const Statistics = () => {
+  const { assignmentData } = useContext(LoaderDataContext || []);
+  console.log(assignmentData);
   return (
-    <div>
-      <h2>From Statistics</h2>
+    <div className="max-w-7xl mx-auto">
+      <h2 className="text-center pt-10 font-extrabold">
+        Assignment marks from assignment 1 to assignment 8.
+      </h2>
+      <div className="flex justify-center">
+        <PieChart width={400} height={400}>
+          <Pie
+            dataKey="marks"
+            isAnimationActive={false}
+            data={assignmentData}
+            cx="50%"
+            cy="50%"
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          ></Pie>
+          <Tooltip />
+        </PieChart>
+      </div>
     </div>
   );
 };
